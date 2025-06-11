1df7e05b785404255fadba87c50654001961b361
yamate-casino <114626397+yamate-casino@users.noreply.github.com>
2025-06-11 02:01:13 +0000

    kk

--- js-index.js
+++ js-index.js
@@ -3,18 +3,27 @@ var url = "";
 var username = '';
 var num = '';
 function load_all(e){
- /*var cookies = document.cookie;
+   url = new URL(window.location.href);
+ var cookies = document.cookie;
  cookies = cookies.split(";")
  for( var cookie of cookies){
    cookie = cookie.split("=");
    var key = cookie[0];
    var value = cookie[1];
    if(key == "username"){
-      //URL作成処理→関数に渡す
+      username = value;
+   }else if(key == "num"){
+      num = value;
    }
- }*/
+ }
+ if(username.length >0 && num.length > 0){
+document.getElementById("b2").innerHTML = username;
+document.getElementById("b2").style.borderBottom = 'solid';
+document.getElementById("b2").style.borderColor = 'white';
+load_only("schedule_now");
+url = url+"?username="+username+"&num="+num;
+ }else{
 //cookieかクエリパラメータで対処
-url =  new  URL(window.location.href);
 var params = url.searchParams;
     try{
     username = params.get("username");
@@ -35,6 +44,7 @@ var params = url.searchParams;
         console.log("パラメータエラー");
         return;
     }
+   }
 }
 function load_only(branch){
       fetch(url,{//クエリパラメータで
