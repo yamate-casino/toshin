15dee1458eff2eaf9fafddb77c69a9188682143a
yamate-casino <114626397+yamate-casino@users.noreply.github.com>
2025-08-15 11:23:32 +0900

    Add files via upload

--- js-schedule.js
+++ js-schedule.js
@@ -50,8 +50,31 @@ for(var i = 1; i<=after_last_date; i++){
     var text = '<div class="b1" onclick=pup('+i+')><p class="date" id="date">'+i+'</p><div class="d" id="d'+i+'"></div></div>';
     document.getElementById("add_d").insertAdjacentHTML("beforeend",text);
 }
-
+var branch = "go";
+function start_ani(){
+    var count = 0;
+    var s = setInterval(()=>{
+        count++;
+        if(count == 1 && branch == "go"){
+            document.getElementById("start_check2").innerHTML = "読込み中...";
+        }else if(count == 2 && branch == "go"){
+            count = 0;
+            document.getElementById("start_check2").innerHTML = "しばらくお待ちください...";
+        }else if(branch == "ok"){
+            document.getElementById("start_check2").style.display = "none";
+            document.getElementById("start_check").style.display = "none";
+        }else if(branch == "no"){
+            document.getElementById("start_check2").innerHTML = "シフト登録の許可がありません";
+            document.getElementById("ca").style.display = "none";
+            document.getElementById("bar").style.display = "none";
+            document.getElementById("top").style.display = "none";
+            clearInterval(s);
+            return;
+        }
+    },2000)
+}
 function start(){
+    start_ani();
     var url1 = url+"?branch=s_check&username="+username;
     fetch(url1,{
         "method":"get",
@@ -64,10 +87,11 @@ function start(){
     })
     .then(json=>{
         if(json == "ok"){
-            
+            branch = "ok";
         }else{
             alert("登録の許可がありません");
             document.getElementById("sub").style.display = "none";
+            branch = "no";
         }
     })
 }
@@ -301,6 +325,7 @@ function w(t){
         fetch(yrl,params);
         alert("送信しました");
     }else if(t == "m"){
+        document.getElementById("pw").innerHTML = "読込み中";
         var data = ["m"];
         var params = {
             "method":"post",
@@ -338,6 +363,7 @@ function w(t){
                     c++;
                 }
                 if(c == 2){
+                    document.getElementById("pw").innerHTML = "シフトの確認";
                     schedule_sudo();
                 }
             })
@@ -347,6 +373,46 @@ function w(t){
         }
         },2000);
         
+    }else if(t == "y"){
+
+    }else if(t == "fs"){
+        console.log(json);
+    }else if(t == "sn"){
+
+var text = '<div class="bar" id="bar"><p id="w"><p><select name="sw" id="se"><option value="選択してください">選択してください</option><option value="月曜">月曜</option><option value="火曜">火曜</option><option value="水曜">水曜</option><option value="木曜">木曜</option><option value="金曜">金曜</option><option value="土曜">土曜</option><option value="日曜">日曜</option></select><select name="st" id="st" ><option value="選択してください">選択してください</option><option value="朝勤務">朝勤務</option><option value="昼勤務">昼勤務</option><option value="夜勤務">夜勤務</option></select><input type="text" value="" id="start_t"><input type="text" value="" id="end_t"><p class="add1" id="add1">追加</p></div><div class="ca" id="ca"><div class="b" id="b"><p>月</p><p>火</p><p>水</p><p>木</p><p>金</p><p>土</p><p>日</p></div><div class="add_d" id="add_d"></div><div class="sub" id="sub">提出</div></div>';
+document.getElementById("blocks").insertAdjacentHTML("before",text);
+var time = new Date();
+var month = time.getMonth()+2;
+var year = time.getFullYear();
+var time2 = new Date(year,month-1,1);
+var first_day = time2.getDay();
+var time3 = new Date(year,month,0);
+var after_last_date = time3.getDate();
+var time4 = new Date(year,month-1,0);
+var before_last_date = time4.getDate(); 
+if(first_day == 0){
+    first_day = 7;
+}
+//first_day = 1;
+
+document.getElementById("month").innerHTML = month+"月";
+
+if(first_day == 1){
+
+}else{
+    for(var a = first_day; a >1; a--){
+        var numb = before_last_date-a+2;
+        var text = '<div class="b1"><p class="date2" id="date">'+numb+'</p><div class="d" id="ddd"></div></div>';
+        document.getElementById("add_d").insertAdjacentHTML("beforeend",text);
+    }
+}
+//<div class="b1" onclick="pup(1)"><p class="date" id="date">1</p><div class="d" id="d1"></div></div>
+for(var i = 1; i<=after_last_date; i++){
+    var text = '<div class="b1" onclick=pup('+i+')><p class="date" id="date">'+i+'</p><div class="d" id="d'+i+'"></div></div>';
+    document.getElementById("add_d").insertAdjacentHTML("beforeend",text);
+}
+
+
     }else if(document.getElementById("start_t").value == "ww"){
         document.getElementById("add_d").remove();
         document.getElementById("b").remove();
@@ -357,15 +423,21 @@ function w(t){
         document.getElementById("add1").remove();
         document.getElementById("sub").remove();
         document.getElementById("w").innerHTML = "管理者ページ";
-        var text = '<p onclick=w("n") class="pw">新規シフト登録の許可</p><p onclick=w("m") class="pw">シフトの確認</p><div id="blocks"></div>';
+        var text = '<p onclick=w("n") class="pw">新規シフト登録の許可</p><p onclick=w("m") class="pw" id="pw">シフトの確認</p><p onclick=w("fs") class="pw" id="pw">確定シフト</p><p onclick=w("sn") class="pw" id="pw">募集人数の設定</p><div id="blocks"></div>';
         document.getElementById("ca").insertAdjacentHTML("beforeend",text);
 
     }
 }
 
 function schedule_sudo(){
+var t1 = '<div id="y" class="y"><p class="y_t">シフト未提出者</p></div>'
+document.getElementById("blocks").insertAdjacentHTML("beforeend",t1);
+for(var p of none){
+    var t2 = '<p>'+p+'</p>';
+    document.getElementById("y").insertAdjacentHTML("beforeend",t2);
+}
 for(var date = 1; date<=31; date++){
-var text = '<div class="su1" id="su1"><p class="su_d">'+date+'日</p><div class="su_block1" id="su_block1"><p class="su_time">9:00～13:00</p><small>シフト希望</small><div class="names" id="names1'+date+'"></div><small>確定シフト</small><div class="names" id="namess1"></div></div><div class="su_block1" id="su_block1"><p class="su_time">14:00～18:00</p><small>シフト希望</small><div class="names" id="names2'+date+'"></div><small>確定シフト</small><div class="names" id="namess2"></div></div><div class="su_block1" id="su_block1"><p class="su_time">18:00～22:00</p><small>シフト希望</small><div class="names" id="names3'+date+'"></div><small>確定シフト</small><div class="names" id="namess3"></div></div><div class="su_block2" id="su_block1"><p class="su_time">その他の時間帯</p><div class="names" id="names4'+date+'"></div></div></div>';
+var text = '<div class="su1" id="su1"><p class="su_d">'+date+'日</p><div class="su_block1" id="su_block1"><p class="su_time">9:00～13:00</p><small>シフト希望</small><div class="names" id="names1'+date+'"></div><small>確定シフト</small><div class="namess" id="namess1'+date+'"></div></div><div class="su_block1" id="su_block1"><p class="su_time">14:00～18:00</p><small>シフト希望</small><div class="names" id="names2'+date+'"></div><small>確定シフト</small><div class="names" id="namess2'+date+'"></div></div><div class="su_block1" id="su_block1"><p class="su_time">18:00～22:00</p><small>シフト希望</small><div class="names" id="names3'+date+'"></div><small>確定シフト</small><div class="names" id="namess3'+date+'"></div></div><div class="su_block2" id="su_block1"><p class="su_time">その他の時間帯</p><div class="names" id="names4'+date+'"></div></div></div>';
 document.getElementById("blocks").insertAdjacentHTML("beforeend",text);
 for(var num = 1; num<=4; num++){
     var text2 = "names"+num+date;
@@ -381,15 +453,47 @@ for(var num = 1; num<=4; num++){
         var count = 0;
         for(var a of schedule_datas[0][date][0][num]){
             count++;
-            var text = '<p class="p1" id="p1'+count+'" onclick=add3('+num+','+count+',0)>'+a+'</p>';
-            document.getElementById(text2).insertAdjacentHTML("beforeend",text);
+            if(a == "募集なし"){
+                var text = '<p class="p2">'+a+'</p>';
+                document.getElementById(text2).insertAdjacentHTML("beforeend",text);                
+            }else{
+                var text = '<p class="p1" id="p'+date+','+num+','+count+'" onclick=add3('+date+','+num+','+count+',0)>'+a+'</p>';
+                document.getElementById(text2).insertAdjacentHTML("beforeend",text);
+            }
         }   
     }
 }
 }
+            var text = '<p class="fix" onclick=fixed()>確定</p>';
+            document.getElementById(text2).insertAdjacentHTML("beforeend",text);
 }
 var count4 = 0;
-function add3(t,n,s){
-    var text = "p"+t+n+s;
-
+var main_data = [{"1":[{"1":[],"2":[],"3":[],"4":[]}],"2":[{"1":[],"2":[],"3":[],"4":[]}],"3":[{"1":[],"2":[],"3":[],"4":[]}],"4":[{"1":[],"2":[],"3":[],"4":[]}],"5":[{"1":[],"2":[],"3":[],"4":[]}],"6":[{"1":[],"2":[],"3":[],"4":[]}],"7":[{"1":[],"2":[],"3":[],"4":[]}],"8":[{"1":[],"2":[],"3":[],"4":[]}],"9":[{"1":[],"2":[],"3":[],"4":[]}],"10":[{"1":[],"2":[],"3":[],"4":[]}],"11":[{"1":[],"2":[],"3":[],"4":[]}],"12":[{"1":[],"2":[],"3":[],"4":[]}],"13":[{"1":[],"2":[],"3":[],"4":[]}],"14":[{"1":[],"2":[],"3":[],"4":[]}],"15":[{"1":[],"2":[],"3":[],"4":[]}],"16":[{"1":[],"2":[],"3":[],"4":[]}],"17":[{"1":[],"2":[],"3":[],"4":[]}],"18":[{"1":[],"2":[],"3":[],"4":[]}],"19":[{"1":[],"2":[],"3":[],"4":[]}],"20":[{"1":[],"2":[],"3":[],"4":[]}],"21":[{"1":[],"2":[],"3":[],"4":[]}],"22":[{"1":[],"2":[],"3":[],"4":[]}],"23":[{"1":[],"2":[],"3":[],"4":[]}],"24":[{"1":[],"2":[],"3":[],"4":[]}],"25":[{"1":[],"2":[],"3":[],"4":[]}],"26":[{"1":[],"2":[],"3":[],"4":[]}],"27":[{"1":[],"2":[],"3":[],"4":[]}],"28":[{"1":[],"2":[],"3":[],"4":[]}],"29":[{"1":[],"2":[],"3":[],"4":[]}],"30":[{"1":[],"2":[],"3":[],"4":[]}],"31":[{"1":[],"2":[],"3":[],"4":[]}]}];
+function add3(d,t,n,s){
+    var text = "p"+d+','+t+','+n;
+    var text2 = "namess"+t+d;
+    var text3 = '<p class="p1" id="p'+d+','+t+','+n+'" onclick=del2('+d+','+t+','+n+',0)>'+document.getElementById(text).innerHTML+'</p>';
+    document.getElementById(text).remove();
+    document.getElementById(text2).insertAdjacentHTML("beforeend",text3);
+    //main_data = JSON.parse(main_data);
+    main_data[0][d][0][t].push(document.getElementById(text).innerHTML);
+}
+function del2(d,t,n,s){
+    var text = "p"+d+','+t+','+n;
+    var text2 = "names"+n+d;
+    var text3 = '<p class="p1" id="p'+d+','+t+','+n+'" onclick=add3('+d+','+t+','+n+',0)>'+document.getElementById(text).innerHTML+'</p>';
+    main_data[0][d][0][t].splice(n-1,1);
+    document.getElementById(text).remove();
+    document.getElementById(text2).insertAdjacentHTML("beforeend",text3);
+}
+function fixed(){
+    console.log(main_data);
+    var params = {
+        "method":"post",
+        "mode":"no-cors",
+        "Content-Type":"application/json",
+        "body":JSON.stringify(main_data)
+    }
+    var url2 = url+"?branch=ficed";
+    fetch(url2,params);
 }
